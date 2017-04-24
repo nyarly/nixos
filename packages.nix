7e6b63b9bcccffe76ac51a77ae4af73c31cd6161
Judson <nyarly@gmail.com>
2017-04-23 18:25:54 -0700

    Setting up split files

---
+++ packages.nix
@@ -0,0 +1,65 @@
+{ config, pkgs, ... }:
+{
+  environment.systemPackages = with pkgs; [
+    # Basic user environment
+    bc
+    chromium
+    fish
+    git
+    neovim
+    tmux
+    tree
+    which
+    vcsh
+
+    # Extra nix tools
+    nix-repl
+    nox
+    patchelf
+
+    # Basic X tools
+    arandr
+    dmenu
+    dunst
+    i3lock
+    i3status
+    libnotify
+    networkmanagerapplet
+    pavucontrol
+    rxvt_unicode
+    trayer
+    urxvt_perls
+    xautolock
+    xlsfonts
+    xsel
+
+    # Firmware tools
+    avrdude
+    avrgcclibc
+    dfu-util
+    usbutils
+
+    # System management
+    linuxPackages.bcc
+    bind
+    btrfs-progs
+    bzip2
+    gzip
+    htop
+    iotop
+    openconnect
+    unzip
+
+    # Programming
+    gnumake
+    go
+    ruby
+    rustc
+    cargo
+
+    # Security
+    gnupg
+    pinentry
+    opensc
+  ];
+}
---
+++ services.nix
@@ -0,0 +1,70 @@
+{ config, pkgs, ... }:
+{
+  # in preference of GPG Agent
+  programs.ssh.startAgent = false;
+  services = {
+    xserver = {
+      enable = true;
+      exportConfiguration = true;
+      displayManager.sddm = {
+        enable = true;
+        autoNumlock = true;
+      };
+      windowManager = {
+        i3.enable = true;
+        xmonad = {
+          enable = true;
+          enableContribAndExtras = true;
+          extraPackages = haskell: [
+            haskell.taffybar
+          ];
+        };
+        default = "i3";
+      };
+      libinput.middleEmulation = true;
+      xkbOptions = "terminate:ctrl_alt_bksp, ctrl:nocaps";
+
+      # videoDrivers = [] # consider adding nvidia, or examining bumble or whatevs
+      multitouch = {
+        enable = true;
+        ignorePalm = true;
+      };
+      synaptics = {
+        enable = true;
+        twoFingerScroll = true;
+        palmDetect = true;
+      };
+    };
+
+    /*
+    # The Profile Sync Daemon
+    psd = {
+    enable = true;
+    users = [ "judson" ];
+    resyncTimer = "30min";
+    };
+    */
+
+    printing = {
+      enable = true;
+    };
+
+    clamav = {
+      updater.enable = true;
+      daemon.enable = true;
+    };
+
+    # Smartcards
+    pcscd.enable = true;
+    locate = {
+      enable = true;
+      localuser = "root";
+    };
+
+    upower.enable = true;
+
+    udev.extraRules = ''
+      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0664", GROUP="wheel", ATTRS{idVendor}=="1050"
+    '';
+  };
+}
