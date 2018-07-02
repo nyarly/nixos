34cbe940ddfcd00b1bf7ae5a0bc5536948ef7043
Judson <nyarly@gmail.com>
2018-07-02 10:11:04 -0700

    Bluetooth

--- neumann.nix
+++ neumann.nix
@@ -1,6 +1,21 @@
 { config, pkgs, ... }:
 {
+  # For John von Neumann, of the von Neumann machine etc etc etc
   networking.hostName = "neumann";
+
+  hardware = {
+    bluetooth.enable = true;
+    pulseaudio.package = pkgs.pulseaudioFull;
+  };
+
+  nixpkgs.overlays = [
+    (self: super: {
+      pulseaudioFull = super.pulseaudioFull.override {
+        x11Support = true;
+        bluetoothSupport = true;
+      };
+    })
+  ];
 
   zramSwap.enable = true;
   services = {
