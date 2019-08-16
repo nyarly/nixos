b5a52b10dc5883fe37fc08eee87f686fa5593576
Judson <nyarly@gmail.com>
2019-08-16 08:09:26 -0700

    Adding cdrom to judson user

--- shared.nix
+++ shared.nix
@@ -58,6 +58,6 @@
     isNormalUser = true;
     uid = 1000;
     shell = "/run/current-system/sw/bin/fish";
-    extraGroups = [ "wheel" "audio" "networkmanager" "docker" "vboxusers" "lp" "dialout" ];
+    extraGroups = [ "wheel" "audio" "cdrom" "networkmanager" "docker" "vboxusers" "lp" "dialout" ];
   };
 }
